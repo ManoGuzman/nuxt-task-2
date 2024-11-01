89d9e2a59d92489b769951de810b398f69819a14
Manuel Guzman <manuguzman8@gmail.com>
2024-10-31 23:45:02 -0600

    Add uttenanc file

---
+++ components/global/utteranc.vue
@@ -0,0 +1,24 @@
+<template>
+</template>
+<script>
+
+export default {
+  layout: "default",
+
+  head: {
+    script: [
+      {
+        src: 'https://utteranc.es/client.js',
+        repo: "ManoGuzman/nuxt-task-2",
+        'issue-term': "pathname",
+        label: "comments",
+        theme: "github-light",
+        crossorigin: "anonymous",
+        body: true,
+        async: true
+      }
+    ]
+  },
+
+};
+</script>
